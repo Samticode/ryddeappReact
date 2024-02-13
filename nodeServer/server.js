402c14d72f73c2f33c969901f8f2cae02c2d5884
Samticode <samti8686@gmail.com>
2024-02-13 14:04:39 +0100

    Add better-sqlite3 dependency and update API endpoint

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const sqlite3 = require('better-sqlite3', { verbose: console.log })
+
+const db = new sqlite3('./database/data.db')
+
 
 
 app.use(cors());
 
 
-app.get('/api', (req, res) => {
-    res.send({ users: ['John', 'Betty', 'Hal'] });
+app.get('/api/all', (req, res) => {
+    const query = `
+        SELECT 
+            Families.FamilyName, 
+            Users.Username, 
+            Users.Email, 
+            Users.IsParent, 
+            Chores.ChoreName, 
+            Chores.Description
+        FROM 
+            Users
+        INNER JOIN 
+            Families ON Users.FamilyID = Families.FamilyID
+        INNER JOIN 
+            Chores ON Users.UserID = Chores.AssignedUserID;
+    `;
+    const data = db.prepare(query).all();
+    res.send(data);
 });
+
 
 
 const port = 3000;
--- rydde-app-react/src/components/Data/Data.js
+++ rydde-app-react/src/components/Data/Data.js
@@ -5,7 +5,7 @@ function Data() {
     const [backendData, setBackendData] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3000/api')
+        fetch('http://localhost:3000/api/all')
             .then(response => response.json())
             .then(data => setBackendData(data))
             .catch(error => console.error('Error:', error));
@@ -13,15 +13,20 @@ function Data() {
 
     return (
         <div>
-            {(typeof backendData.users === 'undefined') ? (
+            {(backendData.length === 0) ? (
                 <p>Loading...</p>
             ) : (
                 <div>
-                    <ul className='user'>
-                        {backendData.users.map((user, index) => (
-                            <li key={index}>{user}</li>
-                        ))}
-                    </ul>
+                    {backendData.map((item, index) => (
+                        <div className='user' key={index}>
+                            <h2>{item.FamilyName}</h2>
+                            <p>Username: {item.Username}</p>
+                            <p>Email: {item.Email}</p>
+                            <p>Is Parent: {item.IsParent ? 'Yes' : 'No'}</p>
+                            <p>Chore: {item.ChoreName}</p>
+                            <p>Description: {item.Description}</p>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
