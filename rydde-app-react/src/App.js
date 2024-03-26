ac91782d6e25bd9c43b013cb46125b193fc3b551
Samticode <samti8686@gmail.com>
2024-03-26 17:01:02 +0100

    Update siteNumber state in App.js

--- rydde-app-react/src/App.js
+++ rydde-app-react/src/App.js
@@ -13,7 +13,7 @@ import Profile from './components/Profile/Profile.jsx';
 
 
 function App() {
-  const [siteNumber, setSiteNumber] = useState(4);
+  const [siteNumber, setSiteNumber] = useState(1);
   let siteComponent = null;
 
   if (siteNumber === 1) {
