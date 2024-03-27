f891acf73f151d45982de7a97db71e30aac97aee
Samticode <samti8686@gmail.com>
2024-03-27 22:36:32 +0100

    Refactor App component to dynamically render different site components

--- rydde-app-react/src/App.js
+++ rydde-app-react/src/App.js
@@ -1,21 +1,22 @@
+// Importing necessary modules and components
 import React, { useState } from 'react';
 import './App.css';
 
 import BackgroundThings from './components/BackgroundThings/BackgroundThings.js';
 import Privacy from './components/Privacy/Privacy.js';
-
 import Navigation from './components/Navigation/Navigation.js';
-
 import Family from './components/FamilySignUpForm/Family.js';
 import User from './components/User/User.js';
 import Homepage from './components/Homepage/Homepage.js';
 import Profile from './components/Profile/Profile.jsx';
 
-
+// Main App component
 function App() {
+  // State variable to keep track of the current site number
   const [siteNumber, setSiteNumber] = useState(1);
   let siteComponent = null;
 
+  // Depending on the siteNumber, different components are rendered
   if (siteNumber === 1) {
     siteComponent = <Family siteNumber={siteNumber} setSiteNumber={setSiteNumber}/>
   } else if (siteNumber === 2) {
@@ -26,23 +27,22 @@ function App() {
     siteComponent = <Profile siteNumber={siteNumber} setSiteNumber={setSiteNumber}/>
   }
 
+  // The rendered JSX
   return (
     <>
+      {/* Navigation component is always rendered */}
       <Navigation siteNumber={siteNumber} setSiteNumber={setSiteNumber} />
+      {/* BackgroundThings component is always rendered */}
       <BackgroundThings />
       
+      {/* Privacy component is rendered only when siteNumber is 1 or 2 */}
       {(siteNumber === 1 || siteNumber === 2) && <Privacy siteNumber={siteNumber} setSiteNumber={setSiteNumber}/>}
 
+      {/* The component determined by the siteNumber state */}
       {siteComponent}
-
-      {/* <div className="color-palet-div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div> */}
     </>
   );
 };
 
+// Exporting the App component to be used in other parts of the application
 export default App;
